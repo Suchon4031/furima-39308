ed257e311b21203926602972b085293f7e4380e5
Suchon4031 <o.h215.su@gmail.com>
2023-05-11 20:56:38 +0900

    userテーブルの作成

--- db/migrate/20230511073017_devise_create_users.rb
+++ db/migrate/20230511073017_devise_create_users.rb
@@ -4,8 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :nickname,           nill: false
+      t.string :email,              null: false, unique: true, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :lastname,           null: false
+      t.string :firstname,          null: false
+      t.string :lastname_kana,      null: false
+      t.string :firstname_kana,     null: false
+      t.date :birth_date,           null: false
 
       ## Recoverable
       t.string   :reset_password_token
