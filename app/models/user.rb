70ad689fc08a1b72c42f0c39bdcf1f59bd0324f0
Suchon4031 <o.h215.su@gmail.com>
2023-05-12 14:33:37 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-   validates :nickname,           presence: true
-   validates :email,              presence: true
-   validates :encrypted_password, presence: true
-   validates :lastname,           presence: true
-   validates :firstname,          presence: true
-   validates :lastname_kana,      presence: true
-   validates :firstname_kana,     presence: true
-   validates :birth_date,         presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :nickname,           presence: true
+  validates :email,              presence: true
+  validates :password,           presence: true, format: {with: PASSWORD_REGEX}
+  validates :lastname,           presence: true, format: {with:/\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :firstname,          presence: true, format: {with:/\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :lastname_kana,      presence: true, format: {with:/\A[ァ-ン]+\z/}
+  validates :firstname_kana,     presence: true, format: {with:/\A[ァ-ン]+\z/}
+  validates :birth_date,         presence: true
 
 end
