c7b37f615abfae7fd99eec6a0c70ec04d589d301
Suchon4031 <o.h215.su@gmail.com>
2023-05-12 23:28:06 +0900

    Faker修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,14 +4,14 @@ FactoryBot.define do
       person {Gimei.name}
     end
 
-    nickname {Faker::Name.initials}
-    email {Faker::Internet.free_email}
-    password {Faker::Internet.password(min_length: 6)}
+    nickname              {Faker::Name.initials}
+    email                 {Faker::Internet.free_email}
+    password              {'1a'+ Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    lastname {person.last.kanji}
-    firstname {person.first.kanji}
-    lastname_kana {person.last.katakana}
-    firstname_kana {person.first.katakana}
-    birth_date {Faker::Date.backward}
+    lastname              {person.last.kanji}
+    firstname             {person.first.kanji}
+    lastname_kana         {person.last.katakana}
+    firstname_kana        {person.first.katakana}
+    birth_date            {Faker::Date.backward}
   end
 end
