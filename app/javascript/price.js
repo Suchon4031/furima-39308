5f55cddf1ae9155513b704ce37be7efbbc87681b
Suchon4031 <o.h215.su@gmail.com>
2023-05-15 22:00:48 +0900

    販売手数料と販売利益の表示

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,6 +1,10 @@
 window.addEventListener('load', () => {
-  console.log("OK");
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.trunc(inputValue * 0.1).toLocaleString();
+    const profitDom = document.getElementById("profit");
+    profitDom.innerHTML = Math.trunc(inputValue * 0.9).toLocaleString();
+  })
 });
-
-const priceInput = document.getElementById("item-price");
-console.log(priceInput);
