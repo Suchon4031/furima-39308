5356c4721e637395103757337672e59727fe331a
Suchon4031 <o.h215.su@gmail.com>
2023-05-23 13:58:59 +0900

    orderscontroller修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: :index
-  before_action :set_item, only: :index, :create
+  before_action :set_item, only: [:index, :create]
 
   def index
     @order_address = OrderAddress.new
